754bf36859be1ab8ed59410e8aec84095f52b20e
Sruthi Sudhakar <jgdss81@gmail.com>
2017-11-05 00:36:32 -0400

    next

---
+++ app/src/main/java/com/example/meena/disastersupporttwo/SheltersTab.java
@@ -0,0 +1,36 @@
+package com.example.meena.disastersupporttwo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by meena on 11/5/2017.
+ */
+
+public class SheltersTab extends Fragment {
+    Hospitals.CommunicationChannel mCallback;
+    public interface CommunicationChannel{
+        // void sendInfo(Double speed, Double distance, Date date);
+        //void graphIt();
+    }
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallback = (Hospitals.CommunicationChannel) activity;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()+ " must implement OnHeadlineSelectedListener");
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.shelters_layout, container, false);
+
+        return rootView;
+    }
+}
